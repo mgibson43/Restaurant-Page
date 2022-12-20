1f2286ae40d0e55b6cdf9bfdb206d3e8511285d4
mgibson <matthew.jgibson43@gmail.com>
2022-12-20 09:34:28 -0500

    Feat: Custom font added, global nav bar added

--- src/index.js
+++ src/index.js
@@ -1,7 +1,38 @@
 import './style.css';
 
+const content = document.getElementById('content');
+const nav = document.createElement('nav');
+const navTitle = document.createElement('h2');
+const navLinks = document.createElement('div');
+const home = document.createElement('div');
+const menu = document.createElement('div');
+const contact = document.createElement('div');
+
+navTitle.classList.add('nav-title');
+navTitle.textContent = 'Kappu Keiki';
+
+navLinks.classList.add('nav-links');
+
+home.classList.add('nav-link');
+home.textContent = 'Home';
+
+menu.classList.add('nav-link');
+menu.textContent = 'Menu';
+
+contact.classList.add('nav-link');
+contact.textContent = 'Contact';
+
+navLinks.appendChild(home);
+navLinks.appendChild(menu);
+navLinks.appendChild(contact);
+
+nav.appendChild(navTitle);
+nav.appendChild(navLinks);
+
+content.appendChild(nav);
+
 function homepage() {
-  const content = document.getElementById('content');
+  
   const header = document.createElement('header');
   const titleBox = document.createElement('div');
   const title = document.createElement('h1');
@@ -11,7 +42,11 @@ function homepage() {
   const quote = document.createElement('p');
 
   // logo.src = ""
+  title.classList.add('title');
   title.textContent = 'Kappu Keiki';
+
+  
+
 
   aboutUs.textContent = 'Kappu Keiki is the best (and only) Japanese style cafe in the city! At Kappu Keiki, we love our otaku and strive to serve the most drinkable coffee and completely eatable bakery products to our customers.'
 
