138ddbcfcfec4099f3262980d7884495f9166622
mgibson <matthew.jgibson43@gmail.com>
2022-12-20 10:11:53 -0500

    Feat: Tabs change page

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './style.css';
 
 const content = document.getElementById('content');
+const container = document.createElement('div');
 const nav = document.createElement('nav');
 const navTitle = document.createElement('h2');
 const navLinks = document.createElement('div');
@@ -8,31 +9,41 @@ const home = document.createElement('div');
 const menu = document.createElement('div');
 const contact = document.createElement('div');
 
+container.classList.add('nav-container');
+
 navTitle.classList.add('nav-title');
 navTitle.textContent = 'Kappu Keiki';
 
 navLinks.classList.add('nav-links');
 
 home.classList.add('nav-link');
+home.classList.add('selected');
 home.textContent = 'Home';
+home.addEventListener('click', homepage);
 
 menu.classList.add('nav-link');
 menu.textContent = 'Menu';
+menu.addEventListener('click', menuPage);
 
 contact.classList.add('nav-link');
 contact.textContent = 'Contact';
+contact.addEventListener('click', contactPage);
 
 navLinks.appendChild(home);
 navLinks.appendChild(menu);
 navLinks.appendChild(contact);
 
-nav.appendChild(navTitle);
-nav.appendChild(navLinks);
+container.appendChild(navTitle);
+container.appendChild(navLinks);
 
-content.appendChild(nav);
+nav.appendChild(container);
+
+content.before(nav);
+const links = document.querySelectorAll('.nav-link');
 
 function homepage() {
-  
+  content.innerHTML = '';
+
   const header = document.createElement('header');
   const titleBox = document.createElement('div');
   const title = document.createElement('h1');
@@ -40,13 +51,17 @@ function homepage() {
   const infoBox = document.createElement('div');
   const aboutUs = document.createElement('p');
   const quote = document.createElement('p');
+  
+
+  links.forEach(link => {
+    link.classList.remove('selected');
+  });
+
+  home.classList.add('selected');
 
   // logo.src = ""
   title.classList.add('title');
   title.textContent = 'Kappu Keiki';
-
-  
-
 
   aboutUs.textContent = 'Kappu Keiki is the best (and only) Japanese style cafe in the city! At Kappu Keiki, we love our otaku and strive to serve the most drinkable coffee and completely eatable bakery products to our customers.'
 
@@ -61,6 +76,26 @@ function homepage() {
   header.appendChild(titleBox);
   header.appendChild(infoBox);
   content.appendChild(header);
+}
+
+function menuPage() {
+  content.innerHTML = '';
+
+  links.forEach(link => {
+    link.classList.remove('selected');
+  });
+
+  menu.classList.add('selected');
+}
+
+function contactPage() {
+  content.innerHTML = '';
+
+  links.forEach(link => {
+    link.classList.remove('selected');
+  });
+
+  contact.classList.add('selected');
 }
 
 homepage();
