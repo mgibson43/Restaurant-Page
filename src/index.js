d12dc66a1217f267917e514f89d9b856a31e11e6
mgibson <matthew.jgibson43@gmail.com>
2022-12-20 11:31:04 -0500

    Update: Hero section added

--- src/index.js
+++ src/index.js
@@ -1,81 +1,74 @@
 import './style.css';
+import heroImg from './hero-img.jpg';
 
 const content = document.getElementById('content');
 const container = document.createElement('div');
-const nav = document.createElement('nav');
-const navTitle = document.createElement('h2');
-const navLinks = document.createElement('div');
+const header = document.createElement('header');
+const headerTitle = document.createElement('h2');
+const headerLinks = document.createElement('div');
 const home = document.createElement('div');
 const menu = document.createElement('div');
 const contact = document.createElement('div');
 
-container.classList.add('nav-container');
+container.classList.add('header-container');
 
-navTitle.classList.add('nav-title');
-navTitle.textContent = 'Kappu Keiki';
+headerTitle.classList.add('header-title');
+headerTitle.textContent = 'Kappu Keiki';
 
-navLinks.classList.add('nav-links');
+headerLinks.classList.add('header-links');
 
-home.classList.add('nav-link');
+home.classList.add('header-link');
 home.classList.add('selected');
 home.textContent = 'Home';
 home.addEventListener('click', homepage);
 
-menu.classList.add('nav-link');
+menu.classList.add('header-link');
 menu.textContent = 'Menu';
 menu.addEventListener('click', menuPage);
 
-contact.classList.add('nav-link');
+contact.classList.add('header-link');
 contact.textContent = 'Contact';
 contact.addEventListener('click', contactPage);
 
-navLinks.appendChild(home);
-navLinks.appendChild(menu);
-navLinks.appendChild(contact);
+headerLinks.appendChild(home);
+headerLinks.appendChild(menu);
+headerLinks.appendChild(contact);
 
-container.appendChild(navTitle);
-container.appendChild(navLinks);
+container.appendChild(headerTitle);
+container.appendChild(headerLinks);
 
-nav.appendChild(container);
+header.appendChild(container);
 
-content.before(nav);
-const links = document.querySelectorAll('.nav-link');
+content.before(header);
+const links = document.querySelectorAll('.header-link');
 
 function homepage() {
   content.innerHTML = '';
 
-  const header = document.createElement('header');
-  const titleBox = document.createElement('div');
-  const title = document.createElement('h1');
-  const logo = document.createElement('img');
-  const infoBox = document.createElement('div');
-  const aboutUs = document.createElement('p');
-  const quote = document.createElement('p');
-  
-
   links.forEach(link => {
     link.classList.remove('selected');
   });
-
   home.classList.add('selected');
 
-  // logo.src = ""
+  const hero = document.createElement('div');
+  const titleBox = document.createElement('div');
+  const title = document.createElement('h1');
+  const titleText = document.createElement('p');
+
+  hero.classList.add('hero-section');
   title.classList.add('title');
+  titleBox.classList.add('title-box');
+  titleText.classList.add('title-text');
+
   title.textContent = 'Kappu Keiki';
-
-  aboutUs.textContent = 'Kappu Keiki is the best (and only) Japanese style cafe in the city! At Kappu Keiki, we love our otaku and strive to serve the most drinkable coffee and completely eatable bakery products to our customers.'
-
-  quote.textContent = '"Omae wa mou shindeiru" - Some buff dude';
-
-  infoBox.appendChild(aboutUs);
-  infoBox.appendChild(quote);
+  titleText.textContent = 'Authentic Japanese Style Cafe';
 
   titleBox.appendChild(title);
-  titleBox.appendChild(logo);
+  titleBox.appendChild(titleText);
 
-  header.appendChild(titleBox);
-  header.appendChild(infoBox);
-  content.appendChild(header);
+  hero.appendChild(titleBox);
+
+  content.appendChild(hero);
 }
 
 function menuPage() {
